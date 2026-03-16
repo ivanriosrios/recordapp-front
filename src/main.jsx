aba42e10126911a95f3129c410a5f082e291f6f9
iarios <ivanargemiroriosrios@gmail.com>
2026-03-15 19:11:29 -0500

    Register service worker for PWA support

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.jsx'
+
+// Registra el service worker generado por vite-plugin-pwa
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
